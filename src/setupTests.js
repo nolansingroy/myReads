bba1209c30cf8fa063e19aa46faaa1732bca9026
nolansingroy <nolansingroy@gmail.com>
2018-09-12 16:00:43 +1000

    docs:changes

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,9 +3,6 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
-/**
- * http://airbnb.io/enzyme/docs/installation/index.html#working-with-react-16
- */
 configure({ adapter: new Adapter() });
 
 // Make Enzyme functions available in all test files without importing
